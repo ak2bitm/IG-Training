db1a7780245650ec611a62d9cce498d34a275fd3
Akhilesh <ak2bitm@gmail.com>
2022-06-07 07:53:34 +0530

    initial commit

---
+++ Java8New Features/LambdaExpression/Employee.java
@@ -0,0 +1,19 @@
+package com.akhilesh.LambdaExpression;
+
+public class Employee {
+
+	private int empId;
+	private String empName;
+	public Employee(int empId, String empName) {
+		super();
+		this.empId = empId;
+		this.empName = empName;
+	}
+	public int getEmpId() {
+		return empId;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	
+}
---
+++ Java8New Features/LambdaExpression/EmployeeMain.java
@@ -0,0 +1,29 @@
+package com.akhilesh.LambdaExpression;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class EmployeeMain {
+
+	public static void main(String[] args) {
+		List<Employee> empList = new ArrayList<Employee>();
+		empList.add(new Employee(101, "Chandu"));
+		empList.add(new Employee(102, "Golu"));
+		empList.add(new Employee(103, "Amit"));
+		empList.add(new Employee(104, "Sumit"));
+		empList.add(new Employee(105, "Ratan"));
+		System.out.println("Original List :");
+		for(Employee e: empList) {
+			System.out.println(e.getEmpId()+" : "+e.getEmpName());
+		}
+		System.out.println("Sort Emp List using Lambda");
+		Collections.sort(empList, (e1,e2)->{
+			return e1.getEmpName().compareTo(e2.getEmpName());
+		});
+		for(Employee e: empList) {
+			System.out.println(e.getEmpId()+" : "+e.getEmpName());
+		}
+	}
+}
